a6e79128b038dae7fd13cd7769f526d73a47f561
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2022-06-17 12:59:17 +0300

    rand square map

--- src/gameMap.cpp
+++ src/gameMap.cpp
@@ -177,9 +177,8 @@ bool InitCreateLabirint(Matrix* matrix, CollidersArray* colArr)
 {
 	srand((unsigned int)time(NULL));
 	int r = rand() % 45;
-	matrix->countCol = r*2+51;
-	r = rand() % 45;
-	matrix->countRow = r*2+51;
+	matrix->countCol = r*2+111;
+	matrix->countRow = r*2+111;
 
 	printf("Labirint with Col:%u and Row:%u\n", matrix->countCol, matrix->countRow);
 	size_t** matr = NULL;
